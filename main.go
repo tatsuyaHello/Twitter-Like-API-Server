1faad3e651357f40bf97a39895c11ad32e3b9cc8
yamamaura_tatusya <t.yamamura@gemcook.com>
2019-07-07 01:25:47 +0900

    [update] Post

--- main.go
+++ main.go
@@ -33,16 +33,6 @@ func dbInit() {
 	db.AutoMigrate(&Post{})
 }
 
-// DB Create
-// func dbInsert(id string, userId string, text string, parentPostId string, commentCount int, postedAt string) {
-// 	db, err := gorm.Open("sqlite3", "post.sqlite3")
-// 	if err != nil {
-// 		panic("You failed to dbInsert")
-// 	}
-// 	defer db.Close()
-// 	db.Create(&Post{ID: id, UserId: userId, Text: text, ParentPostId: parentPostId, CommentCount: commentCount, PostedAt: postedAt})
-// }
-
 // DB Get All
 func dbGetAll() []*Post {
 	db, err := gorm.Open("sqlite3", "post.sqlite3")
@@ -78,148 +68,14 @@ func main() {
 
 		// パスに記述されている post_id を取得している
 		postID := c.Param("post_id")
-		ell := db.Find(&posts, "ParentPostID = ?", postID)
+		ell := db.Find(&posts, "parent_post_id = ?", postID)
 		c.JSON(200, ell)
 	})
 
-	// router.POST("/posts/:path", func(c *gin.Context){
-	// 	path := c.Param("path")
+	// 投稿作成
+	router.POST("/posts/:path", func(c *gin.Context) {
+		if c.Param("path") == "create" {
 
-	// 	if path == "create" {
-	// 		db, err := gorm.Open("sqlite3", "post.sqlite3")
-	// 		if err != nil {
-	// 			panic("You can't open DB (dbInsert())")
-	// 		}
-	// 		defer db.Close()
-	// 		var post Post
-	// 		c.Bind(&post)
-
-	// 		if (post.UserID != "11111111-1111-1111-1111-111111111111") && (post.UserID != "22222222-2222-2222-2222-222222222222") && (post.UserID != "33333333-3333-3333-3333-333333333333") {
-	// 			c.JSON(400, gin.H{
-	// 				"result": "ユーザIDが不適切です",
-	// 			})
-	// 			return
-	// 		}
-
-	// 		validate := validator.New()
-	// 		err2 := validate.Struct(post)
-	// 		fmt.Println(err2)
-	// 		if err2 != nil {
-	// 			c.JSON(400, gin.H{
-	// 				"result": "不正なコンテンツです",
-	// 			})
-	// 			return
-	// 		}
-
-	// 		rand.Seed(time.Now().UnixNano())
-	// 		postRnd := rand.Intn(1000000)
-	// 		post.ID = string(postRnd)
-	// 		post.ParentPostID = ""
-	// 		post.CommentCount = 0
-	// 		post.PostedAt = time.Now()
-
-	// 		db.Create(&post)
-	// 		c.JSON(200, gin.H{
-	// 			"result": "OK",
-	// 		})
-	// 	}else if path == "post_id/comments/create" {
-	// 		db, err := gorm.Open("sqlite3", "post.sqlite3")
-	// 		if err != nil {
-	// 			panic("You can't open DB (dbInsertComment())")
-	// 		}
-	// 		defer db.Close()
-	// 		var post Post
-	// 		c.Bind(&post)
-
-	// 		// ユーザのIDが適切であるかを判断している
-	// 		if (post.UserID != "11111111-1111-1111-1111-111111111111") && (post.UserID != "22222222-2222-2222-2222-222222222222") && (post.UserID != "33333333-3333-3333-3333-333333333333") {
-	// 			c.JSON(400, gin.H{
-	// 				"result": "ユーザIDが不適切です",
-	// 			})
-	// 			return
-	// 		}
-
-	// 		// Textが validation をクリアしているかを判断している
-	// 		validate := validator.New()
-	// 		err2 := validate.Struct(post)
-	// 		fmt.Println(err2)
-	// 		if err2 != nil {
-	// 			c.JSON(400, gin.H{
-	// 				"result": "不正なコンテンツです",
-	// 			})
-	// 			return
-	// 		}
-
-	// 		// データを投入している
-	// 		rand.Seed(time.Now().UnixNano())
-	// 		postRnd := rand.Intn(1000000)
-	// 		post.ID = string(postRnd)
-	// 		post.ParentPostID = c.Param("post_id")
-	// 		err3 := db.Model(&post).Where("id = ?", post.ParentPostID).Update("CommentCount", (post.CommentCount+1))
-	// 		if err3 != nil {
-	// 			c.JSON(400, gin.H{
-	// 				"result": "PostIDが不適切です",
-	// 			})
-	// 			return
-	// 		}
-
-	// 		post.CommentCount = 0
-	// 		post.PostedAt = time.Now()
-
-	// 		db.Create(&post)
-	// 		c.JSON(200, gin.H{
-	// 			"result": "OK",
-	// 		})
-	// 	}
-	// })
-
-	// 新規投稿作成
-	/*
-		router.POST("/posts/create", func(c *gin.Context){
-			fmt.Println("入れたよ〜demo")
-			db, err := gorm.Open("sqlite3", "post.sqlite3")
-			if err != nil {
-				panic("You can't open DB (dbInsert())")
-			  }
-			defer db.Close()
-			var post *Post
-			c.Bind(&post)
-
-			if (post.UserID != "11111111-1111-1111-1111-111111111111") && (post.UserID != "22222222-2222-2222-2222-222222222222") && (post.UserID != "33333333-3333-3333-3333-333333333333") {
-				c.JSON(400, gin.H{
-					"result": "ユーザIDが不適切です",
-				})
-				return
-			}
-
-			validate := validator.New()
-			err2 := validate.Struct(post)
-			if err2 != nil {
-				c.JSON(400, gin.H{
-					"result": "不正なコンテンツです",
-				})
-				return
-			}
-
-			// satoriのuuidを利用して一意な値を生成している
-			uuid, err := uuid.NewV4()
-			fmt.Println(uuid)
-			post.ID = uuid.String()
-			post.ParentPostID = ""
-			post.CommentCount = 0
-			post.PostedAt = time.Now().Format("2018-05-01 11:20:10")
-
-			db.Create(&post)
-			fmt.Println(post.ID)
-			c.JSON(200, gin.H{
-				"result": "OK",
-			})
-		})
-	*/
-
-	// 投稿へのコメント作成
-	router.POST("/posts/:post_id/comments/create", func(c *gin.Context) {
-		if c.Param("post_id") == "create" {
 			fmt.Println("投稿作成の方に入れたよ")
 			db, err := gorm.Open("sqlite3", "post.sqlite3")
 			if err != nil {
@@ -247,18 +103,25 @@ func main() {
 
 			// satoriのuuidを利用して一意な値を生成している
 			uuid, err := uuid.NewV4()
-			fmt.Println(uuid)
 			post.ID = uuid.String()
 			post.ParentPostID = ""
 			post.CommentCount = 0
 			post.PostedAt = time.Now().Format("2018-05-01 11:20:10")
 
 			db.Create(&post)
-			fmt.Println(post.ID)
 			c.JSON(200, gin.H{
 				"result": "OK",
 			})
-		} else {
+		}
+	})
+
+	// 投稿へのコメント作成
+	// /posts/:path と途中まで同様にしてやらないとコンフリクトが返される
+	router.POST("/posts/:path/*action", func(c *gin.Context) {
+		fmt.Println(c.Param("path"))
+		fmt.Println("AAAAAAAAAA")
+		fmt.Println(c.Param("action"))
+		if c.Param("action") == "/comments/create" {
 			fmt.Println("投稿のコメント作成の方へ入れたよ〜")
 			db, err := gorm.Open("sqlite3", "post.sqlite3")
 			if err != nil {
@@ -291,10 +154,7 @@ func main() {
 			uuid, err := uuid.NewV4()
 			post.ID = uuid.String()
 
-			post.ParentPostID = c.Param("post_id")
-			fmt.Println("AAAAAAAAA")
-			fmt.Println(db.Where("id = ?", post.ParentPostID))
-			fmt.Println("BBBBBBBBB")
+			post.ParentPostID = c.Param("path")
 
 			var post2 Post
 			result := db.Where("ID = ?", post.ParentPostID).First(&post2).Update("CommentCount", (post2.CommentCount + 1))
